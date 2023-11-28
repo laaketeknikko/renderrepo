b7afb9b61db9f48b071746a2d72930dc2f73c92e
laaketeknikko <139527598+laaketeknikko@users.noreply.github.com>
2023-11-28 19:58:50 +0200

    added frontend build and use express.static

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ app.use(express.json())
 const cors = require("cors")
 app.use(cors())
 
+app.use(express.static("front"))
+
 const morgan = require("morgan")
 
 app.use(morgan((tokens, req, res) => {
